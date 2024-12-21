d3cad38855adc39c9a4d3431fa9fddcc9a2824be
Irfan Sudarani <irfansudarani.cs23@bmsce.ac.in>
2024-12-21 14:32:26 +0530

    initial commit

---
+++ SubArray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class SubArray{
+    public static void  SubArr(int arr[])
+    {
+        int count=0;
+       
+       for(int i=0;i<arr.length;i++)
+        {
+             int start=i;
+           // System.out.print(cur);
+            for(int j=i;j<arr.length;j++)
+            {
+                int end=j;
+                for(int k=start;k<=end;k++){
+                System.out.print(arr[k]);
+
+            count++;}
+            System.out.println();
+            }
+           
+        }
+        
+        System.out.println("total no of sub array is "+count);
+    }   
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8,10};
+        SubArr(arr);
+
+    }
+}
