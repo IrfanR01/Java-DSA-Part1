e3af259b920de4fcf2de99f68fa8b9b4d796d514
Irfan Sudarani <irfansudarani.cs23@bmsce.ac.in>
2024-12-28 11:24:50 +0530

    initialcommit

--- RainWaterTrapped.java
+++ RainWaterTrapped.java
@@ -1,42 +1,22 @@
-public class RainWaterTrapped {
-    
-    public static void totalwater(int arr[])
-    {
-        int sum=0;
-    
-            int n=arr.length;
-                int left[]=new int[arr.length];
-                int right[]=new int [arr.length];
-
-
-
-                left[0]=arr[0];
-            for(int p=1;p<arr.length;p++)
-            {
-                left[p]=Math.max(left[p-1], arr[p]);
-            }
-
-
-            right[arr.length-1]=arr[arr.length-1];
-            for(int j=arr.length-2;j>=0;j--)
-            {
-                right[j]=Math.max(right[j+1],arr[j]);
-            }
-
-
-            for(int k=0;k<arr.length;k++)
-            {
-                 sum+=(Math.min(right[k],left[k]))-arr[k];
-            }
-            System.out.println("Trapped Water is "+sum);
-           
-             
+class RainWaterTrapped {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+        
+        while (left < right) {
             
+            int area = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, area);
+            
+      
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
         }
         
-    
-    public static void main(String[] args) {
-        int arr[]={4,2,0,6,3,2,5};
-        totalwater(arr);
+        return maxArea;
     }
 }
